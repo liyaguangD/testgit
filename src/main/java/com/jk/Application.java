1ec34bf523d2e3fe74b7ba54c731b12c84d1415f
liyaguangD <1036737208@qq.com>
2018-11-14 19:00:42 +0800

    sadfgh

--- src/main/java/com/jk/Application.java
+++ src/main/java/com/jk/Application.java
@@ -21,7 +21,6 @@ public class Application extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
     @RequestMapping("/")
     String home() {
         return "redirect:book";
